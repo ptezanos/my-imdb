e5be2c3610ea2a9a248cbb670a238f43daefaef8
Pablo T. M <pablo.tezanos@gmail.com>
2023-11-18 15:29:44 +0100

    Project setup

---
+++ src/main/java/com/project/myimdb/MyImdbApplication.java
@@ -0,0 +1,13 @@
+package com.project.myimdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyImdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyImdbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/myimdb/MyImdbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.myimdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyImdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
