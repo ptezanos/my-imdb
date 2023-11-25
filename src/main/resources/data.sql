7a84a343d4946c7c0a9968d8ffe345e32fc5ca97
Pablo T. M <pablo.tezanos@gmail.com>
2023-11-25 18:25:08 +0100

    Add more data to database

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,4 +2,8 @@ INSERT INTO persons (person_name, age, photo_url) VALUES ('Leonardo Dicaprio', '
 INSERT INTO persons (person_name, age, photo_url) VALUES ('Tom Hardy', '30', 'my_url');
 INSERT INTO persons (person_name, age, photo_url) VALUES ('Christopher Nolan', '40', 'my_url');
 
+INSERT INTO persons (person_name, age, photo_url) VALUES ('Joseph Gordon-Levitt', '30', 'my_url');
+INSERT INTO persons (person_name, age, photo_url) VALUES ('Zooey Deschannel', '28', 'my_url');
+
 INSERT INTO movies (title, release_year, director, poster_url) VALUES ('Inception', '2010', 'Christopher Nolan', 'my_url');
+INSERT INTO movies (title, release_year, director, poster_url) VALUES ('(500) Days of Summer', '2010', 'Marc Webb', 'my_url');
