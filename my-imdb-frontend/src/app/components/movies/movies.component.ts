ac17e8c93e68c0bbefd00190d5b85d3613884395
Pablo T. M <pablo.tezanos@gmail.com>
2023-11-28 00:51:26 +0100

    Load Movies in Movies page, simple css

--- my-imdb-frontend/src/app/components/movies/movies.component.ts
+++ my-imdb-frontend/src/app/components/movies/movies.component.ts
@@ -1,17 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavigationButtonComponent } from '../navigation-button/navigation-button.component';
+import { MovieService } from '../../services/movie.service';
+import { Movie } from '../../models';
 
 @Component({
   selector: 'app-movies',
   standalone: true,
   imports: [CommonModule, NavigationButtonComponent],
+  providers: [MovieService],
   templateUrl: './movies.component.html',
   styleUrl: './movies.component.css'
 })
-export class MoviesComponent {
+export class MoviesComponent implements OnInit{
+
+  movieList: Movie[] = [];
 
   homeTitle: string = "HOME";
   homePageName: string = "";
+
+  constructor(private movieService: MovieService) { }
+
+  ngOnInit(): void {
+    this.loadMovies();
+  }
+
+  loadMovies(): void {
+    this.movieService.getMovies().subscribe(
+      (movies) => {
+        this.movieList=movies;
+      }
+    );
+  }
 
 }
