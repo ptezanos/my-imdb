347f317d7e091569b2865461d7b77f263aaad7c6
Pablo T. M <pablo.tezanos@gmail.com>
2023-11-27 23:31:10 +0100

    Update poster urls

--- my-imdb-backend/src/main/resources/data.sql
+++ my-imdb-backend/src/main/resources/data.sql
@@ -5,5 +5,5 @@ INSERT INTO persons (person_name, age, photo_url) VALUES ('Christopher Nolan', '
 INSERT INTO persons (person_name, age, photo_url) VALUES ('Joseph Gordon-Levitt', '30', 'my_url');
 INSERT INTO persons (person_name, age, photo_url) VALUES ('Zooey Deschannel', '28', 'my_url');
 
-INSERT INTO movies (title, release_year, director, poster_url) VALUES ('Inception', '2010', 'Christopher Nolan', 'my_url');
-INSERT INTO movies (title, release_year, director, poster_url) VALUES ('(500) Days of Summer', '2010', 'Marc Webb', 'my_url');
+INSERT INTO movies (title, release_year, director, poster_url) VALUES ('Inception', '2010', 'Christopher Nolan', 'https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._UX500_.jpg');
+INSERT INTO movies (title, release_year, director, poster_url) VALUES ('(500) Days of Summer', '2010', 'Marc Webb', 'https://m.media-amazon.com/images/M/MV5BMTk5MjM4OTU1OV5BMl5BanBnXkFtZTcwODkzNDIzMw@@._UX500_.jpg');
