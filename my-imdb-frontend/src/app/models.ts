bd16096e96a2196a99acb4523250bf5d99168107
Pablo T. M <pablo.tezanos@gmail.com>
2023-11-28 16:52:23 +0100

    Update models

--- my-imdb-frontend/src/app/models.ts
+++ my-imdb-frontend/src/app/models.ts
@@ -4,4 +4,13 @@ export interface Movie {
     release_year: string | null
     director: string | null
     poster_url: string | null
+    cast: Person[] | null
+  }
+
+  export interface Person {
+    id: string | null
+    person_name: string | null
+    age: string | null
+    photo_url: string | null
+    movies: Movie[] | null
   }
